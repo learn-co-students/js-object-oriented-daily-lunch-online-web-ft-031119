e14fdfd0969ce0ae8943678ff60d44a825df4990
Daniel Schneider <daschne8@gmail.com>
2019-06-17 15:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-// global datastore
+let neighborhoodId = 0
+let customerId = 0
+let mealId = 0
+let deliveryId = 0
+
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name
+    this.id = ++neighborhoodId
+  }
+}
+class Meal {
+  constructor(name) {
+    this.name = name
+    this.id = ++mealId
+  }
+}
+class Customer {
+  constructor(name) {
+    this.name = name
+    this.id = ++customerId
+  }
+}
+class Delivery {
+  constructor(name) {
+    this.name = name
+    this.id = ++deliveryId
+  }
+}
